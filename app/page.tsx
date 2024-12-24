71b111d29d72b53add9537ce4d58bf65e2c6499a
Adnan Ayaz <20pwcse1860@uetpeshawar.edu.pk>
2024-12-24 20:51:40 +0500

    Add Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="min-h-screen ">
-      <h1>this is a heading to test the h1</h1>
+    <div className="min-h-screen">
+      <h1>this is a heading</h1>
     </div>
   );
 }
