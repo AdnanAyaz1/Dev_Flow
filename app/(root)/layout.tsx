4a55e3952c302bf53aaa3fa64fd33fac831ca3ee
Adnan Ayaz <20pwcse1860@uetpeshawar.edu.pk>
2024-12-26 18:14:34 +0500

    Implement Right Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
@@ -9,9 +10,11 @@ const layout = ({ children }: { children: ReactNode }) => {
       <div className="flex">
         <LeftSidebar />
 
-        <section className="mx-auto w-full max-w-5xl px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 border-2 border-white ">
+        <section className="mx-auto w-full max-w-5xl px-6 pb-6 pt-36 max-md:pb-14 sm:px-14  ">
           {children}
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );
--- constants/routes.ts
+++ constants/routes.ts
@@ -2,4 +2,6 @@ export const Routes = {
   Home: "/",
   SignIn: "/sign-in",
   SignUp: "/sign-up",
+  PROFILE: (id: string) => `/profile/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
 };
