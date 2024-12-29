a7028b401d7b8e94f637ca88ab4074a2c19e63a9
Adnan Ayaz <20pwcse1860@uetpeshawar.edu.pk>
2024-12-29 19:48:22 +0500

    Setting mongodb and mongoose

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -41,7 +41,6 @@ const QuestionForm = () => {
     e: React.KeyboardEvent<HTMLInputElement>,
     field: { value: string[] }
   ) => {
-    console.log(field, e);
     if (e.key === "Enter") {
       e.preventDefault();
       const tagInput = e.currentTarget.value.trim();
